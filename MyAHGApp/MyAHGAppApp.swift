c77c7cd78697cac5f5b1b30948d24541994bba31
Kamil Stecyk <kamils987@interia.eu>
2023-10-06 08:55:06 +0200

    Initial Commit

---
+++ MyAHGApp/MyAHGAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAHGAppApp.swift
+//  MyAHGApp
+//
+//  Created by Guest User on 06/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyAHGAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
