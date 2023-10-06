24344f88f7e3a6663a7e453361c228484c254e1c
Kamil Stecyk <kamils987@interia.eu>
2023-10-06 09:31:24 +0200

    First lab activity

--- MyAHGApp/ContentView.swift
+++ MyAHGApp/ContentView.swift
@@ -10,10 +10,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image(.AGH)
+                .resizable()
+                .aspectRatio(contentMode: .fit).frame(maxWidth: 300, maxHeight: 150).clipped()
+                
+            Text("Budynek A-0").multilineTextAlignment(.center).padding(1)
+            Text("Dom studencki Olimp").multilineTextAlignment(.center)
+            Text("Mickiewicza 30").multilineTextAlignment(.center)
+            Text("To jest najstarszy budynek nasze pieknej uczelni").multilineTextAlignment(.center).padding(5)
+            Image(.aghMapa)
+            Image(systemName: "figure.roll")
         }
         .padding()
     }
